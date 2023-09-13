853afc33b52549a54f3a3a493fb145e552e37024
Rainata Putra <kriegcom77@gmail.com>
2023-09-13 12:54:19 +0700

    Implementing address controller

--- back-end/app/routes/userRoutes.js
+++ back-end/app/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const cartController = require("../controllers/User/cartController");
+const addressController = require("../controllers/User/addressController");
 
 const router = Router();
 
@@ -8,5 +9,11 @@ router.get("/cart", cartController.getUserCart);
 router.post("/cart", cartController.addItem);
 router.delete("/cart", cartController.deleteItem);
 router.put("/cart", cartController.updateItem);
+
+// address
+router.get("/address", addressController.get);
+router.delete("/address", addressController.delete);
+router.post("/address", addressController.post);
+router.put("/address/:id", addressController.update);
 
 module.exports = router;
